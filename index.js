9364a005b44e8514db37997847460f22317b1b94
MdNaimRipto <naimurtsc567@gmail.com>
2022-11-08 15:49:39 +0600

    srevices added

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config()
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ltefwui.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async () => {
+    try {
+        const servicesCollection = client.db("theFoodieExpress").collection("services")
+        app.get("/services", async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.limit(3).toArray()
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get("/", (req, res) => {
     res.send("The Foodie Express Server is running successfully")
